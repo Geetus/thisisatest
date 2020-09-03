6e139975f49f67174a4295fe0ab4a6e04f7c2213
peter w <peter.whhyte@live.co.uk>
2020-09-03 23:47:26 +1200

    add new test for subtract

--- src/test/java/thisone/TestCalc.java
+++ src/test/java/thisone/TestCalc.java
@@ -6,9 +6,13 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TestCalc{
 
     @Test
-    public void testAdd() {
-  
+    public void testAdd() {  
         assertEquals(4, Calc.Add(2, 2));
-
+    }	
+	
+	    @Test
+    public void testSubtract() {  
+        assertEquals(1, Calc.Subtract(3, 2));
     }
+	
 }
